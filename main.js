beddf11da4168f7874083ccfe13f196782bae88d
Syed Talha <syedt.in00@gmail.com>
2023-11-08 20:12:51 +0500

    uploaded to github

---
+++ main.js
@@ -0,0 +1,13 @@
+let myArray = ["Ali","Ahmed","Talha","Parsa","Anjum"];
+let addFriendInput = document.getElementById("addFriend");
+let addNewBtn= document.getElementById("addNew");
+let table = document.getElementById("output");
+let para = document.getElementById("para");
+
+let a = function(){
+    para.innerText = addFriendInput.value;
+};
+
+
+
+
